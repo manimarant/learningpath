7488f6f2153cc0756222742de64324e23cda1108
Mani <30809968+manimarant@users.noreply.github.com>
2025-09-04 04:45:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,15 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+      server: {
+        host: '0.0.0.0',
+        port: process.env.PORT ? parseInt(process.env.PORT) : 5173
+      },
+      preview: {
+        host: '0.0.0.0',
+        port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+        allowedHosts: 'all' // This disables host checking
       }
     };
 });
